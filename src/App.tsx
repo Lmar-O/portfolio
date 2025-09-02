0725eaf096dfc0bae83e99f653193aafdfe33689
Lmar-O <115730198+Lmar-O@users.noreply.github.com>
2025-09-01 22:47:45 -0500

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
       >
         <Hero />
         <About />
-        <Experience />
+       {/* <Experience /> */}
         <Projects />
         <Contact />
       </motion.main>
